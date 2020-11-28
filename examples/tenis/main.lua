d3e7216f520074d1fb16c2f78d8be6cf239a940c
Thiago Naves <naves.thiago@gmail.com>
2020-11-27 22:19:55 -0300

    Tenis: Add Y ball movement

--- examples/tenis/main.lua
+++ examples/tenis/main.lua
@@ -2,7 +2,6 @@ rx = require'rx'
 require'exhaustMap'
 require'resub'
 
---local scheduler = rx.CooperativeScheduler.create()
 love.keypressed = rx.Subject.create()
 love.keyreleased = rx.Subject.create()
 love.update = rx.Subject.create()
@@ -10,21 +9,23 @@ love.update = rx.Subject.create()
 local screenWidth = 640
 local screenHeight = 480
 
-local bumperSpeed = 300
+local bumperSpeed = 400
 local bumperWidth = 15
 local bumperHeight = 80
-local bumper1x = bumperWidth
-local bumper1y = (screenHeight - bumperHeight) / 2
-local bumper2x = screenWidth - 2 * bumperWidth
-local bumper2y = bumper1y
+local bumper1X = bumperWidth
+local bumper1Y = (screenHeight - bumperHeight) / 2
+local bumper2X = screenWidth - 2 * bumperWidth
+local bumper2Y = bumper1Y
 
+local ballSpeed = 250
 local ballSize = 20
-local ballxInitial = (screenWidth - ballSize) / 2
-local ballyInitial = (screenHeight - ballSize) / 2
-local ballx = ballxInitial
-local bally = ballyInitial
-local ballxSpeed = 300
-local ballySpeed = 300
+local ballMaxSpeedRatio = 3
+local ballXInitial = (screenWidth - ballSize) / 2
+local ballYInitial = (screenHeight - ballSize) / 2
+local ballX = ballXInitial
+local ballY = ballYInitial
+local ballXSpeed = ballSpeed
+local ballYSpeed = 0
 
 function bumperPosFactory(keyUp, keyDown, currPos)
 	return love.keypressed
@@ -41,73 +42,85 @@ function bumperPosFactory(keyUp, keyDown, currPos)
 		end)
 end
 
-function didColide()
-	if bally + ballSize>= bumper1y and bally <= bumper1y + bumperHeight and
-		ballx <= bumper1x + bumperWidth and ballxSpeed < 0 then
+function didColideX()
+	if ballY + ballSize>= bumper1Y and ballY <= bumper1Y + bumperHeight and
+		ballX <= bumper1X + bumperWidth and ballXSpeed < 0 then
 		return true
 	end
 
-	if bally + ballSize >= bumper2y and bally <= bumper2y + bumperHeight and
-		ballx + ballSize >= bumper2x and ballxSpeed > 0 then
+	if ballY + ballSize >= bumper2Y and ballY <= bumper2Y + bumperHeight and
+		ballX + ballSize >= bumper2X and ballXSpeed > 0 then
 		return true
 	end
 
 	return false
 end
 
---[[
-function clamp(val, min, max)
-	if val > max then
-		return max
-	end
-
-	if val < min then
-		return min
-	end
-
-	return val
+function didColideY()
+	return (ballY <= 0 and ballYSpeed < 0) or
+		(ballY + ballSize >= screenHeight and ballYSpeed > 0)
 end
---]]
+
+function setBallDirection()
+	local bumperY = ballX < screenWidth / 2 and bumper1Y or bumper2Y
+	local top = math.max(ballY, bumperY)
+	local distCenter = 2 * math.abs(bumperY + bumperHeight / 2 - top) / bumperHeight
+	local dirX = ballXSpeed ~=0 and ballXSpeed / math.abs(ballXSpeed) or 1
+	local dirY = top < bumperY + bumperHeight / 2 and -1 or 1
+	ballYSpeed = distCenter * ballSpeed * dirY
+end
 
 function love.load()
 	love.window.setMode(screenWidth, screenHeight)
 
-	bumper1yS = bumperPosFactory('w', 's', bumper1y)
-	bumper2yS = bumperPosFactory('up', 'down', bumper2y)
-	bumper1yS:subscribe(function(pos) bumper1y = pos end)
-	bumper2yS:subscribe(function(pos) bumper2y = pos end)
+	bumper1YS = bumperPosFactory('w', 's', bumper1Y)
+	bumper2YS = bumperPosFactory('up', 'down', bumper2Y)
+	bumper1YS:subscribe(function(pos) bumper1Y = pos end)
+	bumper2YS:subscribe(function(pos) bumper2Y = pos end)
 
 	didScoreS = rx.Subject.create()
 	startMovingS = rx.BehaviorSubject.create(1)
-	ballxS = startMovingS:exhaustMap(function()
+	local ballPosS = startMovingS:exhaustMap(function()
 		return love.update
-			:map(function(dt) return dt * ballxSpeed end)
-			:scan(function(acc, new) return acc + new end, ballx)
+			:map(function(dt) return dt * ballXSpeed, dt * ballYSpeed end)
+			:scan(function(acc, newX, newY)
+				acc = acc or {0, 0}
+				acc[1] = acc[1] + newX
+				acc[2] = acc[2] + newY
+				return acc
+			end, {ballX, ballY})
 			:takeUntil(didScoreS)
 			:tap(function()
-				if didColide() then
-					ballxSpeed = -ballxSpeed
+				if didColideX() then
+					ballXSpeed = -ballXSpeed
+					setBallDirection()
+				end
+				if didColideY() then
+					ballYSpeed = -ballYSpeed
 				end
 			end)
-			--:tap(nil, nil, function() print("complete") end)
 	end)
 
-	ballxS:subscribe(function(x) ballx = x end)
+	ballPosS:subscribe(function(pos)
+		ballX = pos[1]
+		ballY = pos[2]
+	end)
 
-	ballxS:subscribe(function(x)
-		if ballx > screenWidth - ballSize or
-			ballx < 0 then
+	ballPosS:subscribe(function(x)
+		if ballX > screenWidth - ballSize or
+			ballX < 0 then
 			print('score')
 			didScoreS(1)
-			ballx = ballxInitial
-			bally = ballyInitial
+			ballX = ballXInitial
+			ballY = ballYInitial
+			ballYSpeed = 0
 			startMovingS(1)
 		end
 	end)
 end
 
 function love.draw()
-	love.graphics.rectangle('fill', ballx, bally, ballSize, ballSize)
-	love.graphics.rectangle('fill', bumper1x, bumper1y, bumperWidth, bumperHeight)
-	love.graphics.rectangle('fill', bumper2x, bumper2y, bumperWidth, bumperHeight)
+	love.graphics.rectangle('fill', ballX, ballY, ballSize, ballSize)
+	love.graphics.rectangle('fill', bumper1X, bumper1Y, bumperWidth, bumperHeight)
+	love.graphics.rectangle('fill', bumper2X, bumper2Y, bumperWidth, bumperHeight)
 end
