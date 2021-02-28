b631422d978af4bc077985d68d943a79a3b935e2
Thiago Naves <naves.thiago@gmail.com>
2021-02-28 16:57:42 -0300

    Break long lorem ipsum lines

--- examples/news_feed_login/main.lua
+++ examples/news_feed_login/main.lua
@@ -187,18 +187,38 @@ end
 -------------------------------------------------
 -- HTTP request mock
 local mock_content = {
-	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc nisl, volutpat id aliquet eu, semper in nisi.',
-	'Maecenas nec ornare libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent mattis ex eget dolor sagittis ornare.',
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nunc nisl, '..
+		'volutpat id aliquet eu, semper in nisi.',
+	'Maecenas nec ornare libero. Class aptent taciti sociosqu ad litora '..
+		'torquent per conubia nostra, per inceptos himenaeos. '..
+		'Praesent mattis ex eget dolor sagittis ornare.',
 	'Morbi imperdiet pharetra arcu.',
-	'Curabitur rhoncus, lectus ac elementum lacinia, ligula elit mollis velit, egestas porttitor nisi dui in eros.',
-	'Nam turpis tellus, malesuada at augue ac, mollis dictum lorem. Morbi mi mi, laoreet ut erat sed, faucibus egestas lorem. Nam sodales lacus nec viverra sagittis.',
-	'Mauris in sodales lorem, non blandit nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean mollis metus eget venenatis venenatis. Mauris elementum cursus rhoncus. Duis at nisl eu dolor congue aliquam.',
-	'Aliquam eu magna vel odio malesuada lacinia et sit amet justo. Curabitur in posuere quam.',
+	'Curabitur rhoncus, lectus ac elementum lacinia, ligula elit mollis velit, '..
+		'egestas porttitor nisi dui in eros.',
+	'Nam turpis tellus, malesuada at augue ac, mollis dictum lorem. Morbi mi mi, '..
+		'laoreet ut erat sed, faucibus egestas lorem. '..
+		'Nam sodales lacus nec viverra sagittis.',
+	'Mauris in sodales lorem, non blandit nulla. Vestibulum ante ipsum primis '..
+		'in faucibus orci luctus et ultrices posuere cubilia curae; '..
+		'Aenean mollis metus eget venenatis venenatis. '..
+		'Mauris elementum cursus rhoncus. Duis at nisl eu dolor congue aliquam.',
+	'Aliquam eu magna vel odio malesuada lacinia et sit amet justo. '..
+		'Curabitur in posuere quam.',
 	'Pellentesque ultricies bibendum sapien ac lobortis. Mauris eget ex augue.',
-	'Phasellus dignissim vitae urna id hendrerit. Maecenas malesuada vulputate arcu a accumsan. Quisque dictum blandit risus, ac consequat lectus scelerisque vitae.',
-	'Duis ac gravida velit. Nulla lectus ipsum, ullamcorper a nulla sed, volutpat blandit ipsum. Donec cursus tellus ut vestibulum posuere.',
-	'Vestibulum nec odio sed magna venenatis porttitor ac ut metus. Vivamus eu tortor eget est venenatis lacinia. Mauris aliquet nunc ut velit sollicitudin luctus. Curabitur iaculis commodo enim, nec volutpat libero sollicitudin id. Phasellus nec cursus tortor. Donec ultrices, justo at pharetra laoreet, lacus dui blandit risus, quis vehicula augue justo nec lacus.',
-	'Phasellus varius pulvinar tristique. Fusce mi arcu, venenatis eu nulla at, fringilla porta turpis. Praesent commodo condimentum risus, id lobortis ex. Ut eget nisl ligula.',
+	'Phasellus dignissim vitae urna id hendrerit. '..
+		'Maecenas malesuada vulputate arcu a accumsan. '..
+		'Quisque dictum blandit risus, ac consequat lectus scelerisque vitae.',
+	'Duis ac gravida velit. Nulla lectus ipsum, ullamcorper a nulla sed, '..
+		'volutpat blandit ipsum. Donec cursus tellus ut vestibulum posuere.',
+	'Vestibulum nec odio sed magna venenatis porttitor ac ut metus. '..
+		'Vivamus eu tortor eget est venenatis '..
+		'lacinia. Mauris aliquet nunc ut velit sollicitudin luctus. '..
+		'Curabitur iaculis commodo enim, nec volutpat libero sollicitudin id. '..
+		'Phasellus nec cursus tortor. Donec ultrices, justo at pharetra laoreet, '..
+		'lacus dui blandit risus, quis vehicula augue justo nec lacus.',
+	'Phasellus varius pulvinar tristique. Fusce mi arcu, venenatis eu nulla at, '..
+		'fringilla porta turpis. Praesent commodo condimentum risus, '..
+		'id lobortis ex. Ut eget nisl ligula.',
 }
 
 local mock_content_steps = {3, 5, 3} -- how many posts to add on each reply
